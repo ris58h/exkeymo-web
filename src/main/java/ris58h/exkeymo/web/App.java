6d19f5d6aac3b1928fc10fadc22bfa4d3f2d7b67
Ilya Rodionov <ris58h@gmail.com>
2020-10-04 19:37:08 +0300

    Better start page

--- src/main/java/ris58h/exkeymo/web/App.java
+++ src/main/java/ris58h/exkeymo/web/App.java
@@ -10,59 +10,54 @@ import java.util.concurrent.Executors;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //TODO
         int port = 80;
-        int threads = 2;
+        int threads = Runtime.getRuntime().availableProcessors();
 
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         server.setExecutor(Executors.newFixedThreadPool(threads));
 
-        byte[] indexHtmlBytes = App.class.getResourceAsStream("/index.html").readAllBytes();
-        server.createContext("/", exchange -> {
-            if (!"GET".equals(exchange.getRequestMethod())) {
-                exchange.sendResponseHeaders(404, -1);
-                return;
-            }
-
-            exchange.getResponseHeaders().set("Content-Type", "text/html");
-            exchange.sendResponseHeaders(200, indexHtmlBytes.length);
-            OutputStream os = exchange.getResponseBody();
-            os.write(indexHtmlBytes);
-            os.close();
-        });
-
+        byte[] htmlBytes = App.class.getResourceAsStream("/index.html").readAllBytes();
         AppBuilder appBuilder = new AppBuilder();
         appBuilder.init();
-        server.createContext("/build", exchange -> {
-            if (!"POST".equals(exchange.getRequestMethod())) {
-                exchange.sendResponseHeaders(404, -1);
+        server.createContext("/", exchange -> {
+            if ("GET".equals(exchange.getRequestMethod())) {
+                exchange.getResponseHeaders().set("Content-Type", "text/html");
+                exchange.sendResponseHeaders(200, htmlBytes.length);
+                OutputStream os = exchange.getResponseBody();
+                os.write(htmlBytes);
+                os.close();
                 return;
             }
 
-            String layout = parseLayoutFromRequest(new String(exchange.getRequestBody().readAllBytes()));
-            if (layout == null) {
-                exchange.sendResponseHeaders(400, -1);
-                return;
-            }
+            if ("POST".equals(exchange.getRequestMethod())) {
+                String layout = parseLayoutFromRequest(new String(exchange.getRequestBody().readAllBytes()));
+                if (layout == null) {
+                    exchange.sendResponseHeaders(400, -1);
+                    return;
+                }
 
-            byte[] bytes;
-            try {
-                bytes = appBuilder.buildApp(layout);
-            } catch (Exception e) {
-                bytes = ("Processing error: " + e.getMessage()).getBytes(StandardCharsets.UTF_8);
-                exchange.sendResponseHeaders(500, bytes.length);
+                byte[] bytes;
+                try {
+                    bytes = appBuilder.buildApp(layout);
+                } catch (Exception e) {
+                    bytes = ("Processing error: " + e.getMessage()).getBytes(StandardCharsets.UTF_8);
+                    exchange.sendResponseHeaders(500, bytes.length);
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(bytes);
+                    os.close();
+                    return;
+                }
+
+                exchange.getResponseHeaders().set("Content-Type", "application/vnd.android.package-archive");
+                exchange.getResponseHeaders().set("Content-Disposition", "attachment; filename=\"Custom Keyboard Layout.apk\"");
+                exchange.sendResponseHeaders(200, bytes.length);
                 OutputStream os = exchange.getResponseBody();
                 os.write(bytes);
                 os.close();
                 return;
             }
 
-            exchange.getResponseHeaders().set("Content-Type", "application/vnd.android.package-archive");
-            exchange.getResponseHeaders().set("Content-Disposition", "attachment; filename=\"Custom Keyboard Layout.apk\"");
-            exchange.sendResponseHeaders(200, bytes.length);
-            OutputStream os = exchange.getResponseBody();
-            os.write(bytes);
-            os.close();
+            exchange.sendResponseHeaders(404, -1);
         });
 
         server.start();

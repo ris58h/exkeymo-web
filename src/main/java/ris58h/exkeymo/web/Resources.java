70b62f02b4af3a357ccb9b6ed62117c6209afa25
Ilya Rodionov <ris58h@gmail.com>
2022-12-01 20:28:54 +0300

    Split the main page into Complex and Docs. Stub for Simple.

--- src/main/java/ris58h/exkeymo/web/Resources.java
+++ src/main/java/ris58h/exkeymo/web/Resources.java
@@ -3,10 +3,10 @@ package ris58h.exkeymo.web;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Utils {
+public class Resources {
     public static byte[] readAllBytes(Class<?> clazz, String name) throws IOException {
         try (InputStream is = clazz.getResourceAsStream(name)) {
-            return is.readAllBytes();
+            return is == null ? null : is.readAllBytes();
         }
     }
 }
